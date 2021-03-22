a7169d042fc84cba7b36a033208d7fd5e6ad78e9
Pythux <pythux@nowhere.org>
2021-03-22 15:07:52 +0100

    words and spécial caracters

--- compiler/tokensLex.py
+++ compiler/tokensLex.py
@@ -8,21 +8,18 @@
 
 from ply import lex
 
+from boa import boa
+
 
 # List of token names.   This is always required
-tokens = (
+tokens = [
     'Plus',
     'Minus',
     'Multiply',
     'Divide',
     'LPAREN',
     'RPAREN',
-
-    'Word',
-    'RefJump',
-    'Number',
-    'Indent',
-    )
+]
 
 # Regular expression rules for simple tokens
 t_Plus = r'\+'
@@ -31,8 +28,14 @@ t_Multiply = r'\*'
 t_Divide = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-t_Word = r'[a-zA-Z_]\S*'
-# \S Matches anything other than a space, tab or newline.
+
+
+# add functions:
+tokens += [
+    'Number',
+    'Indent',
+    'RefJump',
+]
 
 
 # A regular expression rule with some action code
@@ -55,6 +58,52 @@ def t_RefJump(t):
     return t
 
 
+reserved = {
+    # new ones (or witch change)
+    'if': 'If',
+    'jump': 'Jump',
+    'def': 'Function',
+    'return': 'FunReturn',
+
+    'not': 'NotEqual',
+
+    # asm reserved
+    'end': 'EndProg',
+    'op': 'AsmOpération'
+}
+tokens += list(reserved.values())
+tokens += ['Variable']  # words by default
+
+reservedSpecial = {
+    '==': 'Equal',
+    '===': 'StrictEqual',
+    '!=': 'NotEqual',
+    '>': 'GreaterThan',
+    '>=': 'GreaterThanOrEqual',
+    '<': 'LowerThan',
+    '<=': 'LowerThanOrEqual',
+}
+tokens += (reservedSpecial.values())
+
+
+# function starting with t_ will be run for tokens even if not in tokens list
+def t_Word(t):
+    r'[a-zA-Z_]\S*'  # \S Matches anything other than a space, tab or newline.
+    # print(t.type) == 'Word'
+    t.type = reserved.get(t.value, 'Variable')  # Check for reserved words
+    return t
+
+
+startReservedSpecial = boa(list(reservedSpecial.keys())).map(lambda w: w[0])
+
+
+def t_SpecialWord(t):
+    r'\S+'  # \S Matches anything other than a space, tab or newline
+    if t.value[0] in startReservedSpecial:
+        t.type = reservedSpecial[t.value]  # Check for reserved words
+        return t
+
+
 # A string containing ignored characters (spaces and tabs)
 t_ignore = ''
 
@@ -68,6 +117,7 @@ def t_error(t):
         t.lexer.skip(1)
 
 
+tokens = list(set(tokens))
 # Build the lexer
 lexer = lex.lex()
 
@@ -75,9 +125,10 @@ lexer = lex.lex()
 def test():
     # Test it out
     data = '''
+    <
     if true jump
         yé
-            yo
+            4 >= 12
 
     #Ref: yoél
     iftruejump
