7029fd05e9484fb17cac6a6a9be2a2779014db99
Pythux <pythux@nowhere.org>
2021-03-28 22:54:33 +0200

    clean

--- compiler/yacc/grammar/__init__.py
+++ compiler/yacc/grammar/__init__.py
@@ -4,11 +4,6 @@ import pkgutil
 
 # import all submodule of grammar in alphabetical order
 __all__ = []
-# for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-#     __all__.append(module_name)
-#     _module = loader.find_module(module_name).load_module(module_name)
-#     globals()[module_name] = _module
-
 import importlib
 
 
