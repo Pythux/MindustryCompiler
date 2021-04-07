594f8c73dfa893dde01dabc3858440ea8fe14dfa
Pythux <pythux@nowhere.org>
2021-04-08 00:33:20 +0200

    wip conserving ID / info manipulation on lines

---
+++ compiler/yacc/classes/AsmInst.py
@@ -0,0 +1,16 @@
+
+
+class AsmInst:
+    def __init__(self) -> None:
+        pass
+
+    # return list of ID
+    def getVars(self):
+        pass
+
+    # replace an ID by another (ID or info)
+    def replace(self, var, toReplaceBy):
+        pass
+
+    def toLine(self):
+        pass
